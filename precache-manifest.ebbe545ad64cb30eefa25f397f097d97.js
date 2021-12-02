12c65dafae18a71c604059613e849796f7bd3bed
sachinidilanka31 <sachini@sachini.co.uk>
2021-12-02 22:24:56 +0000

    Updates

---
+++ precache-manifest.ebbe545ad64cb30eefa25f397f097d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6af538db597fc6d981c7",
+    "url": "/algolia/static/css/main.d491ec95.chunk.css"
+  },
+  {
+    "revision": "6af538db597fc6d981c7",
+    "url": "/algolia/static/js/main.6af538db.chunk.js"
+  },
+  {
+    "revision": "0ce9ffce426b05e65fd8",
+    "url": "/algolia/static/css/1.b0b5571e.chunk.css"
+  },
+  {
+    "revision": "0ce9ffce426b05e65fd8",
+    "url": "/algolia/static/js/1.0ce9ffce.chunk.js"
+  },
+  {
+    "revision": "fa9b375d5008490c821f",
+    "url": "/algolia/static/js/runtime~main.fa9b375d.js"
+  },
+  {
+    "revision": "e76f271a0c42152553e756ca47f847af",
+    "url": "/algolia/index.html"
+  }
+];
